8151dffaefc0fd322c84b9c1e6961272536ee7c9
ravi-kolla <ravitejakolla29@gmail.com>
2023-05-29 22:17:11 -0400

    jobs component updated

--- layouts/components/JobPost.js
+++ layouts/components/JobPost.js
@@ -1,23 +1,25 @@
 import { storyblokEditable } from "@storyblok/react";
+import { useState } from "react";
 
-const JobPost = ({ post }) => {
+const JobPost = ({ post, jobSelected, showDetails }) => {
 
-    const handleClick = (e) => {
-        console.log("job post clicked");
-    }
+    const handleClick = () => {
+        showDetails(post);
+    };
 
     return (
-        <>
-            <div class="bg-white  shadow-xl shadow-gray-100 flex flex-col sm:flex-row gap-3 justify-between px-5 py-4 rounded-md mb-2" onClick={() => handleClick()}>
-                <div class="text-left">
-                    <p class="text-purple-800 text-sm">{post?.category}</p>
-                    <h5 class="font-bold mt-px">{post?.title}</h5>
-                    <div class="flex items-center gap-3 mt-2">
-                        <span class="bg-purple-100 text-purple-700 rounded-full px-3 py-1 text-sm">{post?.jobType}</span>
-                        <span class="text-slate-600 text-sm flex gap-1 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+        <>  
+        <a href={`#${post.jobId}`}>
+            <div className={`job-post bg-white shadow-xl shadow-gray-100 flex flex-col sm:flex-row gap-3 justify-between px-5 py-4 rounded-md mb-2 ${jobSelected? "job-selected" : ""}`} role="button" onClick={() => handleClick()}>
+                <div className="text-left">
+                    <p className="ink text-sm">{post?.category}</p>
+                    <h5 className="font-bold mt-px">{post?.title}</h5>
+                    <div className="flex items-center gap-3 mt-2">
+                        <span className="lemon-bg ink rounded-full px-3 py-1 text-sm">{post?.jobType}</span>
+                        <span className="text-slate-600 text-sm flex gap-1 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                         {post?.location}
                         </span>
@@ -26,6 +28,7 @@ const JobPost = ({ post }) => {
                 <div>
                 </div>
             </div>
+            </a>
         </>
         );
 };
--- layouts/components/Jobs.js
+++ layouts/components/Jobs.js
@@ -1,15 +1,56 @@
 import { storyblokEditable } from "@storyblok/react";
+import { useState } from "react";
 import JobPost from "./JobPost";
+import { render } from 'storyblok-rich-text-react-renderer';
 
 const Jobs = ({blok}) => {
 
+    const [jobDetails, setJobDetails] = useState(blok?.post[0]);
+    const [jobSelected, setJobSelected] = useState(blok?.post[0].title);
+
+    const showDetails = (post) => {
+        setJobSelected(post.title);
+        setJobDetails(post);
+    };
+
     return (
       <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-6 flex-col">
-        <div class="w-1/3 relative flex flex-col jus  overflow-hidden bg-gray-50 p-6 sm:py-12">
+        <div className="container mx-auto md:flex px-5 py-6 flex-row">
+          <div className="w-full md:w-1/3 relative flex flex-col overflow-hidden bg-gray-50 p-6 sm:py-12">
           {blok.post.map((item) => (
-            <JobPost key={item.title} post={item}/>
+            <JobPost key={item.title} post={item} showDetails={showDetails} jobSelected={item.title == jobSelected ? true : false } />
           ))}
+          </div>
+          <div className="w-full md:w-2/3 relative flex flex-col jus  overflow-hidden bg-gray-50 p-6 sm:py-12">
+          { jobDetails && <div id={jobDetails.jobId} className="bg-white p-4">
+            <div className="row">
+                <div className="col-md-12  ">
+                    <h4 className="position-title">{jobDetails.title}</h4>
+                </div>
+            </div>
+            <div className="row   ">
+                <div className="col-md-12  ">
+                    <p>
+                        <span>{jobDetails.location}</span>
+                    </p>
+                </div>
+            </div>
+            <hr />
+            <div className="row   ">
+                <div className="col-md-12  ">
+                    <div>
+                    <div className="row pt-5">
+                        <div className="col-md-12  ">
+                            <h5>Job Description</h5>
+                        </div>
+                    </div>
+                    <div className="row text-left pt-2">
+                        {render(jobDetails.description)}
+                    </div>
+                    </div>
+                </div>
+            </div>
+          </div> }
           </div>
         </div>
       </section>
