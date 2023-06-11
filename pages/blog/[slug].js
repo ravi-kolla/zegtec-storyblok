e9beb126b33b6e15b914b68c6885792d65637148
ravi-kolla <ravitejakolla29@gmail.com>
2023-06-11 03:12:29 -0400

    updated blog

---
+++ pages/blog/[slug].js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { getBlogPostBySlug } from '../../lib/useStoryblok'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import sanitizeHtml from 'sanitize-html';
+import { renderRichText, getStoryblokApi } from "@storyblok/react";
+import Base from "@layouts/Baseof";
+import config from "@config/config.json";
+
+
+export default function BlogPost() {
+  const [blogPost, setBlogPost] = useState(null)
+  const router = useRouter()
+  const { slug } = router.query
+  const storyblokApi = getStoryblokApi();
+  let renderedRichText;
+  useEffect(() => {
+    async function fetchBlogPost() {
+        const { data } = await storyblokApi.get(`cdn/stories/blog-posts/${slug}`, {
+            version: 'draft',
+          })
+          const post = data.story
+          setBlogPost(post)
+    }
+
+    if (slug) {
+      fetchBlogPost()
+      console.log(blogPost);
+      renderedRichText = blogPost && renderRichText(blogPost.content.content);
+
+    }
+  }, [slug])
+
+  if (!blogPost) {
+    return (
+        <Base>
+            <section className="section blog-post">
+                <div className="text-center">Loading...</div>
+            </section>
+        </Base>
+    )
+  }
+  const { title } = config.site;
+  return (
+    <Base title={title}>
+    <section class="section blog-post">
+        <div class="container">
+        <h1>{blogPost.content.title}</h1>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {blogPost.content.content}
+        </ReactMarkdown>
+        <div>{renderedRichText}</div>
+    </div>
+    </section>
+    </Base>
+
+  )
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,44 @@
-import BlogPagination, { getStaticProps } from "./page/[slug]";
+import { useEffect, useState } from 'react'
+import { getBlogPosts } from '../../lib/useStoryblok'
+import Base from "@layouts/Baseof";
+import config from "@config/config.json";
 
-export { getStaticProps };
-export default BlogPagination;
+export default function Blog() {
+  const [blogPosts, setBlogPosts] = useState([])
+
+  useEffect(() => {
+    async function fetchBlogPosts() {
+      const posts = await getBlogPosts()
+      setBlogPosts(posts)
+    }
+
+    fetchBlogPosts(blogPosts);
+    console.log(blogPosts);
+  }, [])
+  const { title } = config.site;
+
+  return (
+    <Base title={title}>
+        <section class="section">
+            <div class="container">
+                <div className="grid gap-12 sm:grid-cols-2 lg:grid-cols-4">
+                {blogPosts.map((post) => (
+                    <div key={post.slug}>
+                    {/* Render other fields of the blog post */}
+                    <article className="max-w-xs">
+                        <a href={`blog/${post.slug}`}>
+                            <img src={post.content.image.filename} class="mb-5 rounded-lg" alt="Image 1" />
+                        </a>
+                        <h2 className="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+                            <a href={`blog/${post.slug}`}>{post.content.title}</a>
+                        </h2>
+                        <p className="mb-4 font-light text-gray-500 dark:text-gray-400">{post.content.highlights}</p>
+                    </article>
+                    </div>
+                ))}
+                </div>
+            </div>
+        </section>
+    </Base>
+  )
+}
